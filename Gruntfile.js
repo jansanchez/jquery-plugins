0bda30d43a3d32fe0edcbbce4882ffb32160b5f3
Jan Sanchez <joejansanchez@gmail.com>
2014-03-01 18:36:13 -0500

    add stylus watcher

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,9 +9,9 @@ module.exports = function(grunt) {
           bare: true
         },
         expand: true,
-        cwd: 'coffee/',
+        cwd: 'frontend/coffee/',
         src: ['*.coffee','!_*.coffee'],
-        dest: 'js/',
+        dest: 'public/js/',
         ext: '.js'
       }
     },
@@ -20,21 +20,28 @@ module.exports = function(grunt) {
       options: {
         jshintrc : '.jshintrc'
       },
-      js: ['js/*.js']
+      js: ['public/js/*.js']
     },
     //watch
     /*echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p*/
     watch: {
       coffee: {
-        files: 'coffee/*.coffee',
+        files: 'frontend/coffee/*.coffee',
         tasks: ['coffee', 'jshint'],
         options: {
           interrupt: true
         }
       },
       jade: {
-        files: ['jade/*.jade'],
+        files: ['frontend/jade/*.jade'],
         tasks: ['jade'],
+        options: {
+          interrupt: true
+        }
+      },
+      stylus: {
+        files: ['frontend/styl/*.styl'],
+        tasks: ['stylus'],
         options: {
           interrupt: true
         }
@@ -48,13 +55,28 @@ module.exports = function(grunt) {
         },
         files: [{
           expand: true,
-          cwd: 'jade/',
+          cwd: 'frontend/jade/',
           src: [ '**/*.jade', '!config/*.jade', '!layout/*.jade', '!render/*.jade'],
           dest: 'html',
           ext: '.html'
         }]
       }
     },
+    //stylus
+    stylus: {
+      compile: {
+        options: {
+          paths: ['frontend/stylus']
+        },
+        files: [{
+          expand: true,
+          cwd: 'frontend/styl/',
+          src: [ '**/*.styl'],
+          dest: 'public/css',
+          ext: '.css'
+        }]
+      }
+    }
   });
 
   //loadNpmTasks
@@ -62,6 +84,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
 
   // Run Default task(s).
   grunt.registerTask('default', ['coffee', 'jshint']);
