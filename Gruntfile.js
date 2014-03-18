5bc6fb3f5b82beeca9e4ebd0f99ef8bb63c72ee6
Jan Sanchez <joejansanchez@gmail.com>
2014-03-18 16:08:53 -0500

    add Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
         files: 'frontend/coffee/**/*.coffee',
         tasks: ['coffee', 'jshint', 'jasmine:phantomjs'],
         options: {
-          interrupt: false
+          interrupt: true
         }
       },
       jade: {
